c286bd3d158dbb0fbb42858f06769fc4947eecee
sankalpsid19 <sankalpyadav96@gmail.com>
2025-03-14 13:21:00 +0530

    config file update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/sankalp_portfolio',
+  base: process.env.VITE_BASE_PATH||'/react-vite-deploy',
 })
